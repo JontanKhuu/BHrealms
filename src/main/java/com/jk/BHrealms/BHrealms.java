1c871b115df6bf7a5e664e9c10d672335a722bbd
Jontan Khuu <jontank424@gmail.com>
2023-07-10 13:10:44 -0400

    first commit

--- src/main/java/com/jk/BHrealms/BHrealms.java
+++ src/main/java/com/jk/BHrealms/BHrealms.java
@@ -1,7 +1,18 @@
 package com.jk.BHrealms;
 
+import com.jk.BHrealms.Init.BlockInit;
+import com.jk.BHrealms.Init.ItemInit;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-
-@Mod()p
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+@Mod(BHrealms.MODID)
 public class BHrealms {
+    public static final String MODID = "bhrealms";
+
+    public BHrealms(){
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInit.ITEM.register(bus);
+        BlockInit.BLOCKS.register(bus);
+    }
 }
--- src/main/java/com/jk/BHrealms/Init/ItemInit.java
+++ src/main/java/com/jk/BHrealms/Init/ItemInit.java
@@ -4,7 +4,6 @@ import com.jk.BHrealms.BHrealms;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEM = DeferredRegister.create(ForgeRegistries.ITEMS, BHrealms.MODID);
