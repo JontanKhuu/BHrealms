f8f256f81e3b5a86ed9e826563e85df107cfebd3
Jontan Khuu <jontank424@gmail.com>
2023-07-18 21:04:04 -0400

    fixed right arm not working

--- src/main/java/com/jk/BHrealms/item/client/GladArmorModel.java
+++ src/main/java/com/jk/BHrealms/item/client/GladArmorModel.java
@@ -8,16 +8,16 @@ import software.bernie.geckolib.model.GeoModel;
 public class GladArmorModel extends GeoModel<GladArmorItem> {
     @Override
     public ResourceLocation getModelResource(GladArmorItem animatable) {
-        return new ResourceLocation(BHrealms.MODID, "geo/glad_helmet.geo.json");
+        return new ResourceLocation(BHrealms.MODID, "geo/glad_armor.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(GladArmorItem animatable) {
-        return new ResourceLocation(BHrealms.MODID, "textures/armor/glad_helm.png");
+        return new ResourceLocation(BHrealms.MODID, "textures/armor/glad_texture.png");
     }
 
     @Override
     public ResourceLocation getAnimationResource(GladArmorItem animatable) {
-        return new ResourceLocation(BHrealms.MODID, "animations/glad_helm.animation.json");
+        return new ResourceLocation(BHrealms.MODID, "animations/glad_armor.animation.json");
     }
 }
